eabf9164245ebcea1291da70ab45ec26bf33843b
Vini <vinitran124@gmail.com>
2022-07-28 02:18:32 +0700

    update 28/7

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,50 @@
 import React from 'react'
 import '../css/Home.css'
 import Navbar from './Navbar'
+import Card from 'react-bootstrap/Card';
+import Carousel from 'react-bootstrap/Carousel';
 import { Products } from './Products'
 
+const SlideImage = () => {
+    return (
+        <>
+            <div className="wrapper-slide">
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="./images/poster1.jpg"
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="./images/poster2.jpg"
+                            alt="Second slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="./images/poster3.jpg"
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </>
+    )
+}
 export const Home = () => {
     return (
         <div className="wrapper">
             <Navbar />
-            <Products />
+            <div>
+                <SlideImage />
+                <Products />
+            </div>
+
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShop } from '@fortawesome/free-solid-svg-icons'
@@ -8,32 +8,45 @@ function Navbar() {
   return (
     <div className="navbox">
       <div className="leftside">
-        <Link to='/' className='link'>
-          <h1 className="logo">Shoppe</h1>
+        <Link to='/' className='logo link'>
+          <div >Shoppe</div>
         </Link>
         <div className="wrapper-nav-item">
           <Link to='/' className="link nav-item">
-            <h1 className="item">Home</h1>
+            <div className="item">Home</div>
           </Link>
           <div className="nav-item">
             <div className="dropdown">
               <button className="dropbtn">
-                <h1>Products</h1>
+                <div className="item">Products</div>
               </button>
               <div className="dropdown-content">
-                <a href="#">Smartphone</a>
-                <a href="#">Laptop</a>
-                <a href="#">Headphone</a>
+                <a href={`/typeProduct=${"Smartphone"}`}>Smartphone</a>
+                <a href={`/typeProduct=${"Laptop"}`}>Laptop</a>
+                <a href={`/typeProduct=${"Headphone"}`}>Headphone</a>
               </div>
             </div>
           </div>
           <Link to='/contact' className="link nav-item">
-            <h1 className="item">Contact</h1>
+            <div className="item">Contact</div>
           </Link>
         </div>
 
       </div>
       <div className="rightside">
+        {/* <div className="nav-item">
+          <div className="dropdown">
+            <button className="dropbtn">
+              <div className="item">Trần Thành Vinh</div>
+            </button>
+            <div className="dropdown-content">
+              <a href="/profile">My Profile</a>
+              <a href="/cart">My Cart</a>
+              <a href="/myProduct">My Product</a>
+              <a href="/">Log Out</a>
+            </div>
+          </div>
+        </div> */}
         <Link to='/signup' className='link'>
           <button type="button" className="button">SIGN UP</button>
         </Link>
